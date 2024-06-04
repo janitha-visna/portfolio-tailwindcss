9194a722273b90522c71bd041d9191c6968b1602
janitha-visna <visnajanitha@gmail.com>
2024-06-04 22:30:04 +0530

    dark mode added

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
+const moon = document.querySelector("#moon");
+const body = document.querySelector("body");
+const hLink = document.querySelectorAll("#hLink");
 
 hamburger.addEventListener("click", () => {
-  menu.classList.toggle("hidden")
-  hamburger.classList.toggle("bg-white")
-  hamburger.classList.toggle("bg-white")
+  menu.classList.toggle("hidden");
+  hamburger.classList.toggle("bg-white");
+});
+
+hLink.forEach((hLink) => {
+  hLink.addEventListener("click", () => {
+    menu.addEventListener.toggle("hidden");
+    hamburger.classList.toggle("bg-white");
+  });
+});
+
+moon.addEventListener("click", () => {
+  body.classList.toggle("dark");
 });
